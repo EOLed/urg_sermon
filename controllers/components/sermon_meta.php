561095aeebdbbd2851f0d5064c7dbc522a779b94
Amos Chan <amos.chan+github@chapps.org>
2011-07-04 19:30:10 -0400

    SermonMeta imports BaseSermonComponent correctly

--- controllers/components/sermon_meta.php
+++ controllers/components/sermon_meta.php
@@ -1,5 +1,5 @@
 <?php
-App::import("Component", "UrgSermon.BaseSermonComponent");
+App::import("Component", "UrgSermon.BaseSermon");
 class SermonMetaComponent extends BaseSermonComponent {
     function build($widget_id) {
         parent::build($widget_id);
